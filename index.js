9c505000216667d61e541e29aa2bfd5e843282df
Tomas Carignano <tomascarignano98@gmail.com>
2021-07-24 14:09:00 -0300

    Fixed problem of storing leads across page refreshes.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const myLeads = [];
+let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 inputBtn.addEventListener("click", function () {
   if (inputEl.value) {
     myLeads.push(inputEl.value);
-    renderLeads();
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+    renderLeads();
   }
 });
 
